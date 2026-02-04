83eda0486fefc6deaaa28feafb561378f3ba02f9
bielwolf <gabryellobo@gmail.com>
2026-02-04 10:25:58 -0300

    feat: Adiciona modelos Estudante e Curso, configurações de admin e serializers

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Estudante(models.Model):
+    nome = models.CharField(max_length=100, null=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    cpf = models.CharField(max_length=11, unique=True, null=False)
+    data_nascimento = models.DateField(null=False)
+    numero_telefone = models.CharField(max_length=15, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+NIVEL = [
+    ('B', 'Básico'),
+    ('I', 'Intermediário'),
+    ('A', 'Avançado'),
+]
+class Curso(models.Model):
+    codigo = models.CharField(max_length=10, unique=True, null=False)
+    descricao = models.TextField(null=False, blank=False)
+    nivel = models.CharField(max_length=1, choices=NIVEL, null=False, default='B')
+
+    def __str__(self):
+        return self.codigo
