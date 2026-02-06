bf1656229aa0e04921b90ed753453325aff41593
bielwolf <gabryellobo@gmail.com>
2026-02-06 09:37:43 -0300

    refactor: refatorar a estrutura do código e otimiza performance em múltiplos módulos

--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'escola',
     'rest_framework',
+    'django_filters', 
 ]
 
 MIDDLEWARE = [
@@ -131,4 +132,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
+
+
+
+
+
